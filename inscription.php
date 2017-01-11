5b8f35366f3552034e823175d83590b2a4181548
WHOK-S <valentin.marlier@gmail.com>
2017-01-11 13:55:30 +0100

    done: html, todo: php

---
+++ inscription.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="index_css/inscription.css">
+</head>
+<body>
+   <div class="header">
+           <div class="connexion"><a href="" target="_self">Connexion</a>/<a href="" target="_self">Inscription</a></div>
+           <div class="navbar">
+               <a href="">Univers</a>
+               <a href="">Actualités</a>
+               <a href=""><img src="" alt="">SeaLanders Logo</a>
+               <a href="">Forum</a>
+               <a href="">Shop</a>
+           </div>
+        </div>
+        
+        
+   <div class="main">
+      
+       <div class="inscription">
+            <div class="reseaux">Inscrivez-vous avec:</div>
+            <div class="inscription_0">
+                   <a class="fb" href=""><img src="/img/facebook.png" alt="Facebook" width="50px"></a>
+                   <a class="tw" href=""><img src="/img/twitter.png" alt="Twitter" width="50px"></a>
+                   <a class="g" href=""><img src="/img/google.png" alt="Google +" width="50px"></a>
+            </div>
+            <div class="info">
+            </div>
+       </div>
+       
+       <div class="connexion">Connexion</div>
+   </div>
+   
+   
+   <footer class="footer_paul">
+       <div class="footer_0">
+            <div class="newsletter">
+                <form action="" method="post">
+                    <p>Newsletter</p>
+                    <input type="email" placeholder="sealanders@mail.fr">
+                    <button type="submit">Envoyer</button>
+                </form>
+            </div>
+            <div class="support_faq">
+                <div><a href="">Support Techniques</a></div>
+                <div><a href="">F.A.Q</a></div>
+            </div>
+        </div>
+        <div class="footer_1">
+            <div><a href="">A propos</a></div>
+            <div><a href="">Confidentialité</a></div>
+            <div><a href="">Conditions Générale</a></div>
+        </div>
+        <div class="link">
+            <a href=""><img src="/img/facebook.png" alt="Facebook" width="50px"></a>
+            <a href=""><img src="/img/twitter.png" alt="Twitter" width="50px"></a>
+        </div>
+    </footer>
+</body>
+</html>
