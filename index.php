34a64e17dd214e7d44b90a4a91c31f29d54a9174
WHOK-S <valentin.marlier@gmail.com>
2017-01-11 13:51:49 +0100

    done: html homepage, todo: php homepage

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="index_css/index.css">
+</head>
+<body>
+    <div class="header">
+       <div class="connexion"><a href="" target="_self">Connexion</a>/<a href="" target="_self">Inscription</a></div>
+       <div class="navbar">
+           <a href="">Univers</a>
+           <a href="">Actualités</a>
+           <a href=""><img src="" alt="">SeaLanders Logo</a>
+           <a href="">Forum</a>
+           <a href="">Shop</a>
+       </div>
+    </div>
+    <div class="main">
+        <div class="left_side"> <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim dolore nemo libero consequatur. Voluptate natus libero assumenda porro. Maiores debitis laudantium fugit harum repellat voluptate dolorem quia assumenda delectus placeat. </p>
+        </div>
+        <div class="carousel"> 
+            <div class="telecharger">
+                <a href="">Télécharger le jeu ici</a></div>
+            </div>
+        <div class="right_side"> <p>PUB ! PUB ! PUB !</p>
+        </div>
+    </div>
+    <div class="main_1">
+        <div class="who">
+            <h2>Qui sommes nous ?</h2>
+            <article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, tempore atque cupiditate id facere ullam. Earum molestiae exercitationem sint quisquam, nobis ea alias placeat inventore minus vitae, quibusdam qui beatae.</article>
+        </div>
+    </div>
+    
+    <footer class="footer_paul">
+       <div class="footer_0">
+            <div class="newsletter">
+                <form action="" method="post">
+                    <p>Newsletter</p>
+                    <input type="email" placeholder="sealanders@mail.fr">
+                    <button type="submit">Envoyer</button>
+                </form>
+            </div>
+            <div class="support_faq">
+                <div><a href="">Support Techniques</a></div>
+                <div><a href="">F.A.Q</a></div>
+            </div>
+        </div>
+        <div class="footer_1">
+            <div><a href="">A propos</a></div>
+            <div><a href="">Confidentialité</a></div>
+            <div><a href="">Conditions Générale</a></div>
+        </div>
+        <div class="link">
+            <a href=""><img src="/img/facebook.png" alt="Facebook" width="50px"></a>
+            <a href=""><img src="/img/twitter.png" alt="Twitter" width="50px"></a>
+        </div>
+    </footer>
+</body>
+</html>
