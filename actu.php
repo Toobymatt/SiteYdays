bc24213998db454e793cd7d802d6cfb7e9cc8124
WHOK-S <valentin.marlier@gmail.com>
2017-01-11 14:00:37 +0100

    done:html,todo:php

---
+++ actu.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Actualité</title>
+    <link rel="stylesheet" href="index_css/actu.css">
+</head>
+<body>
+    <div class="header">
+           <div class="connexion"><a href="" target="_self">Connexion</a>/<a href="" target="_self">Inscription</a></div>
+           <div class="navbar">
+               <a href="">Univers</a>
+               <a href="">Actualités</a>
+               <a href=""><img src="" alt="">SeaLanders Logo</a>
+               <a href="">Forum</a>
+               <a href="">Shop</a>
+           </div>
+        </div>
+        
+    <div class="main">
+        <div class="maj">
+            <article class="art_1">
+                <h2>Dernière Mise à Jour</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel non facilis expedita delectus porro, saepe temporibus dolor reprehenderit autem cupiditate excepturi placeat amet quam minus, quaerat iusto praesentium tempore doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quos error, expedita asperiores hic. Non sint ipsam earum reprehenderit ullam iste, quos autem unde consequuntur qui, minus debitis cupiditate dignissimos.</p>
+            </article>
+            <div class="img">image<img src="" alt=""></div>
+        </div>
+        <div class="bas">
+            <div class="left">
+                <p>Fil D'actualité</p>
+                <div class="fil_actu">
+                    <div class="img">image</div>
+                    <div class="text">
+                    <h3>titre</h3>
+                    <p>Infos sur l'article</p>
+                    </div>
+                </div>
+                <div class="fil_actu">
+                    <div class="img">image</div>
+                    <div class="text">
+                    <h3>titre</h3>
+                    <p>Infos sur l'article</p>
+                    </div>
+                </div>
+                <div class="fil_actu">
+                    <div class="img">image</div>
+                    <div class="text">
+                    <h3>titre</h3>
+                    <p>Infos sur l'article</p>
+                    </div>
+                </div>
+                <div class="fil_actu">
+                    <div class="img">image</div>
+                    <div class="text">
+                    <h3>titre</h3>
+                    <p>Infos sur l'article</p>
+                    </div>
+                </div>
+            </div>
+            <div class="pub">
+                <p>PUB ! PUB ! PUB !</p>
+            </div>
+        </div>
+    </div>       
+      
+     
+    
+    <footer class="footer_paul">
+       <div class="footer_0">
+            <div class="newsletter">
+                <form action="" method="post">
+                    <p>Newsletter</p>
+                    <input type="email" placeholder="sealanders@mail.fr">
+                    <button type="submit">Envoyer</button>
+                </form>
+            </div>
+            <div class="support_faq">
+                <div><a href="">Support Techniques</a></div>
+                <div><a href="">F.A.Q</a></div>
+            </div>
+        </div>
+        <div class="footer_1">
+            <div><a href="">A propos</a></div>
+            <div><a href="">Confidentialité</a></div>
+            <div><a href="">Conditions Générale</a></div>
+        </div>
+        <div class="link">
+            <a href=""><img src="/img/facebook.png" alt="Facebook" width="50px"></a>
+            <a href=""><img src="/img/twitter.png" alt="Twitter" width="50px"></a>
+        </div>
+    </footer>
+</body>
+</html>
